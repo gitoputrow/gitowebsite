966ec0cebba7afb2b7a60a5cb4b3c8b8384461ba
gitopw <gito.wardana@nusantarainfrastructure.com>
2026-06-20 00:12:04 +0700

    revamp view

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 'use client'
 import "./globals.css";
-import { usePathname } from 'next/navigation';
 import { Plus_Jakarta_Sans } from 'next/font/google'
-import Navbar from "../../public/components/navbar/navbar";
 
 const jakarta = Plus_Jakarta_Sans({
   subsets: ['latin'],
@@ -15,16 +13,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const pathname = usePathname();
-
-  // Tentukan halaman mana yang tidak ingin tampilkan Navbar
-  const hideNavbar = pathname.startsWith('/my-craft/');
-
   return (
     <html lang="en" className={`${jakarta.variable}`}>
       
       <body >
-        {/* {!hideNavbar && <Navbar />} */}
         {children}
       </body>
     </html>
